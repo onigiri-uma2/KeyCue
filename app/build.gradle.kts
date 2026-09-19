7383fffa571bae8b00e30a4f79d8c93e92fbc38a
onigiri-uma2 <onigiri.uma2@gmail.com>
2026-09-19 18:45:59 +0900

    chore: バージョンを1.3 (versionCode 3) に更新

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.onigiri.keycue"
         minSdk = 29
         targetSdk = 37
-        versionCode = 2
-        versionName = "1.2"
+        versionCode = 3
+        versionName = "1.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
