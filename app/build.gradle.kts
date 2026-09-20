62c5907ffb93c4e80281263720210206690f5302
onigiri-uma2 <onigiri.uma2@gmail.com>
2026-09-20 09:37:30 +0900

    chore: バージョンを1.5 (versionCode 5) に更新

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.onigiri.keycue"
         minSdk = 29
         targetSdk = 37
-        versionCode = 4
-        versionName = "1.4"
+        versionCode = 5
+        versionName = "1.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
