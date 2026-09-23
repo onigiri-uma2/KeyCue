83e195d2ee471c52aadb44cb7d1907c941f3e6c6
onigiri-uma2 <onigiri.uma2@gmail.com>
2026-09-23 14:16:19 +0900

    bump: バージョンを1.7 (versionCode 7) に更新

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.onigiri.keycue"
         minSdk = 29
         targetSdk = 37
-        versionCode = 6
-        versionName = "1.6"
+        versionCode = 7
+        versionName = "1.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
